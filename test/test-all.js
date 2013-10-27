9f05aaf6d8c4b984cb6a446896621ef073c95298
cmfatih <cmuratfatih@gmail.com>
2013-10-26 21:46:27 -0400

    Add: test-all.js

---
+++ test/test-all.js
@@ -0,0 +1,19 @@
+/*
+ * App
+ * Copyright (c) 2013 Author (Url)
+ * For the full copyright and license information, please view the LICENSE.txt file.
+ */
+
+// Init reqs
+
+// Init global vars
+
+// Init global funcs
+
+// for tidy output
+function tidyOutput(iText) {
+  return "\r\n========== " + (iText + '') + " ==========\r\n";
+}
+
+// Tests
+console.log(tidyOutput("TESTS: test-all.js"));
