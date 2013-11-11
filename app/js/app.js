a40eaf09b7244d61564d222e3930d10b0cb7ac40
cmfatih <cmuratfatih@gmail.com>
2013-11-10 22:10:09 -0500

    Update: Change util provider and add tidyTime, tidyLog

--- app/js/app.js
+++ app/js/app.js
@@ -93,50 +93,57 @@ app.config(['$routeProvider', function($routeProvider) {
 // App providers
 
 // Util provider
-app.provider('util', function() {
+app.factory('util', function() {
   // return for factory
   return {
-    $get: ['$injector', function($injector) {
-      return {
-        gotoUrl: function(iUrl, iParam) {
-          console.log(iUrl);
-          // Check params
-          if(iUrl === undefined) return false;
+    
+    // Tidy time
+    tidyTime: function tidyTime() {
+      return new Date(new Date().toISOString().replace("Z", "+0" + (new Date().getTimezoneOffset()/60) + ":00")).toISOString().replace("T", " ").substr(0, 19);
+    },
 
-          // Init vars
-          var iTarget         = 'self',
-              iConfirm        = false,
-              iConfirmMsg     = ''
-          ;
+    // Tidy log
+    tidyLog: function tidyLog(iStr, iOut) {
+      var returnRes         = {"time": this.tidyTime(), "message": null};
+          returnRes.message = (iStr && typeof console !== "undefined") ? iStr : null;
 
-          // Init params
-          if(iParam !== undefined) {
-            if(iParam.target !== undefined)     iTarget     = iParam.target;
-            if(iParam.confirm !== undefined)    iConfirm    = iParam.confirm;
-            if(iParam.confirmMsg !== undefined) iConfirmMsg = iParam.confirmMsg;
-          }
+      return (iOut === undefined || iOut === true) ? console.log(returnRes) : returnRes;
+    },
 
-          // Confirmation
-          if(iConfirm === true) {
-            var TCM = confirm(iConfirmMsg);
+    // Goto url
+    gotoUrl: function gotoUrl(iUrl, iParam) {
 
-            if(TCM === false) {
-              return false;
-            }
-          }
+      // Check params
+      if(iUrl === undefined) return false;
 
-          // window open
-          if(iTarget == '_blank') {
-            window.open(iUrl, '_blank');
-          }
-          else {
-            window.open(iUrl, '_self');
-          }
+      // Init vars
+      var iTarget         = 'self',
+          iConfirm        = false,
+          iConfirmMsg     = ''
+      ;
 
-          return true;
-        }
-      };
-    }]
+      // Init params
+      if(iParam !== undefined) {
+        if(iParam.target !== undefined)     iTarget     = iParam.target;
+        if(iParam.confirm !== undefined)    iConfirm    = iParam.confirm;
+        if(iParam.confirmMsg !== undefined) iConfirmMsg = iParam.confirmMsg;
+      }
+
+      // Confirmation
+      if(iConfirm === true) {
+        if(!confirm(iConfirmMsg)) return false;
+      }
+
+      // goto url
+      if(iTarget == '_blank') {
+        window.open(iUrl, '_blank');
+      }
+      else {
+        document.location.href = iUrl;
+      }
+
+      return true;
+    }
   };
 });
 
