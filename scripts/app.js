b695411b357af6809d94db5aead00e7ed23bccb0
cmfatih <cmuratfatih@gmail.com>
2013-12-03 16:31:36 -0500

    Move app.js to services/httpd

--- scripts/app.js
+++ scripts/app.js
@@ -1,144 +1,15 @@
 #!/usr/bin/env node
 
 // Init reqs
-var mFS         = require('fs'),    // fs module
-    mHapi       = require('hapi'),  // hapi module
-    mPath       = require('path')   // path module
+var mFS           = require('fs'),    // fs module
+    mPath         = require('path')   // path module
 ;
 
-// Init global vars
-var gPathCur,     // current path
-    gPathScrFile, // script file path
-    gPathScrDir,  // script path
-    gConfig,      // config
-    gConfigError, // config error
-    gArgs,        // arguments
-    gArgsCnt,     // arguments count
-    gEnvNode,     // environment
-    gServer,      // http server
-    gRoutes       // routes
+// Init vars
+var gPathSep      = mPath.sep,              // path separator
+    gPathCur      = mFS.realpathSync('.'),  // current path
+    gPathScrFile  = __filename,             // script file path
+    gPathScrDir   = __dirname               // script path
 ;
 
-gPathCur        = mFS.realpathSync('.') + mPath.sep;
-gPathScrFile    = __filename;
-gPathScrDir     = __dirname;
-gConfig         = {"configFile": null};
-gConfigError    = null;
-gArgs           = process.argv;
-gArgsCnt        = gArgs.length;
-gEnvNode        = (process.env.NODE_ENV !== undefined) ? process.env.NODE_ENV : 'unknown';
-
-// Init Argv
-if(gArgsCnt > 2) {
-  for(var i = 2; i < gArgsCnt; i++) {
-    switch(gArgs[i]) {
-      case "-c":
-      case "--configFile":
-        gConfig.configFile  = (gArgs[++i] + '').trim();
-        break;
-      default:
-        console.log("Invalid argument! (" + gArgs[i] + ")");
-        process.exit(0);
-        break;
-    }
-  }
-}
-
-// Check config
-if(gConfig.configFile !== null) {
-  if(mFS.existsSync(gConfig.configFile)) {
-    try {
-      gConfig       = JSON.parse(mFS.readFileSync(gConfig.configFile)); //+++ Check exists members of gConfig
-    }
-    catch(e) {
-      gConfigError  = 'Invalid configuration file. (' + e + ')';
-    }
-  }
-  else {
-    gConfigError    = 'Configuration file could not be read.';
-  }
-}
-
-if(gConfigError === null) {
-  // Check config paramaters such as server port etc...
-}
-
-if(gConfigError !== null) {
-  console.log('Configuraton error! (' + gConfigError + ')');
-
-  process.exit(0);
-}
-
-// Init global funcs
-
-// Tidy time
-function tidyTime() {
-  return new Date(new Date().toISOString().replace("Z", "+0" + (new Date().getTimezoneOffset()/60) + ":00")).toISOString().replace("T", " ").substr(0, 19);
-}
-
-// Console log
-function tidyLog(iStr, iOut) {
-  var returnRes         = {"time": tidyTime(), "message": null};
-      returnRes.message = (iStr && typeof console !== "undefined") ? iStr : null;
-
-  return (iOut === undefined || iOut === true) ? console.log(returnRes) : returnRes;
-}
-
-// Global config
-gConfig = {
-  hapi: {
-    server: {
-      port: 12080
-    },
-    yar: {
-      options: {
-        name: 'appsess',
-        maxCookieSize: 0,
-        cookieOptions: {
-          password: 'cOOkIEPaSSWoRD',
-          isSecure: false //+++ Take from env or config file
-        }
-      }
-    }
-  }
-};
-
-// Create server
-gServer = new mHapi.createServer('localhost', gConfig.hapi.server.port);
-
-// Init yar plugin
-gServer.pack.allow({ext: true}).require('yar', gConfig.hapi.yar.options, function(err) {
-  if(err) {
-    tidyLog('Yar plugin could not be initialized! (' + err + ')');
-    throw err;
-  }
-});
-
-// Init server routes
-
-// Default route for static files
-gRoutes = [
-  {
-    method: 'GET',
-    path: '/{path*}',
-    handler: {
-      directory: {
-        listing: false,
-        index: true,
-        path: function(request) {
-          //tidyLog('request.params:' + JSON.stringify(request.params));  // for debug
-          //tidyLog('request.path:' + JSON.stringify(request.path));      // for debug
-
-          return gPathScrDir + '/../app';
-        }
-      }
-    }
-  }
-];
-
-// Start server
-gServer.route(gRoutes);
-
-gServer.start(function() {
-  tidyLog('Server is listening on port ' + gConfig.hapi.server.port);
-});
+console.log('\u001B[2J\u001B[0;0f');        // clear console
---
+++ services/httpd/httpd.js
@@ -0,0 +1,141 @@
+#!/usr/bin/env node
+
+// Init reqs
+var mFS           = require('fs'),    // fs module
+    mHapi         = require('hapi'),  // hapi module
+    mPath         = require('path')   // path module
+;
+
+// Init global vars
+var gPathSep      = mPath.sep,              // path separator
+    gPathCur      = mFS.realpathSync('.'),  // current path
+    gPathScrFile  = __filename,             // script file path
+    gPathScrDir   = __dirname,              // script path
+    gConfig       = {"configFile": null},   // config
+    gConfigError  = null,                   // config error
+    gArgs         = process.argv,           // arguments
+    gArgsCnt      = gArgs.length,           // arguments count
+    gServer       = null,                   // http server
+    gRoutes       = null,                   // routes
+    gEnvNode      = (process.env.NODE_ENV !== undefined) ? process.env.NODE_ENV : null // environment
+;
+
+// Init Argv
+if(gArgsCnt > 2) {
+  for(var i = 2; i < gArgsCnt; i++) {
+    switch(gArgs[i]) {
+      case "-c":
+      case "--configFile":
+        gConfig.configFile  = (gArgs[++i] + '').trim();
+        break;
+      default:
+        console.log("Invalid argument! (" + gArgs[i] + ")");
+        process.exit(0);
+        break;
+    }
+  }
+}
+
+// Check config
+if(gConfig.configFile !== null) {
+  if(mFS.existsSync(gConfig.configFile)) {
+    try {
+      gConfig       = JSON.parse(mFS.readFileSync(gConfig.configFile)); //+++ Check exists members of gConfig
+    }
+    catch(e) {
+      gConfigError  = 'Invalid configuration file. (' + e + ')';
+    }
+  }
+  else {
+    gConfigError    = 'Configuration file could not be read.';
+  }
+}
+
+if(gConfigError === null) {
+  // Check config paramaters such as server port etc...
+}
+
+if(gConfigError !== null) {
+  console.log('Configuraton error! (' + gConfigError + ')');
+
+  process.exit(0);
+}
+
+// Init global funcs
+
+// Tidy time
+function tidyTime() {
+  return new Date(new Date().toISOString().replace("Z", "+0" + (new Date().getTimezoneOffset()/60) + ":00")).toISOString().replace("T", " ").substr(0, 19);
+}
+
+// Console log
+function tidyLog(iStr, iOut) {
+  var returnRes         = {"time": tidyTime(), "message": null};
+      returnRes.message = (iStr && typeof console !== "undefined") ? iStr : null;
+
+  return (iOut === undefined || iOut === true) ? console.log(returnRes) : returnRes;
+}
+
+// Console clear
+function tidyClear() {
+  console.log('\u001B[2J\u001B[0;0f');
+}
+
+
+// Global config
+gConfig = {
+  hapi: {
+    server: {
+      port: 12080
+    },
+    yar: {
+      options: {
+        name: 'appsess',
+        maxCookieSize: 0,
+        cookieOptions: {
+          password: 'cOOkIEPaSSWoRD',
+          isSecure: false
+        }
+      }
+    }
+  }
+};
+
+// Create server
+gServer = new mHapi.createServer('localhost', gConfig.hapi.server.port);
+
+// Init yar plugin
+gServer.pack.allow({ext: true}).require('yar', gConfig.hapi.yar.options, function(err) {
+  if(err) {
+    tidyLog('Yar plugin could not be initialized! (' + err + ')');
+    throw err;
+  }
+});
+
+// Init server routes
+
+// Default route for static files
+gRoutes = [
+  {
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+      directory: {
+        listing: false,
+        index: true,
+        path: function(request) {
+          //tidyLog('request.params:' + JSON.stringify(request.params));  // for debug
+          //tidyLog('request.path:' + JSON.stringify(request.path));      // for debug
+          return gPathScrDir + '/../../app';
+        }
+      }
+    }
+  }
+];
+
+// Start server
+gServer.route(gRoutes);
+
+gServer.start(function() {
+  tidyLog('Server is listening on port ' + gConfig.hapi.server.port);
+});
